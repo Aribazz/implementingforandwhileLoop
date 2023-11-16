68c402e4cd394b04f2ff0fba96b22ad15d47d709
Ajayi Ariyo Sunday <106662907+Aribazz@users.noreply.github.com>
2023-11-16 08:37:02 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// let number = 0
+// while(number <=12){
+//   number = number + 2
+//   console.log(number);
+// }
+for (let number = 0; number <= 12; number = number + 2) {
+  console.log(number);
+}
+function App() {
+  return <div></div>;
+}
+export default App;
